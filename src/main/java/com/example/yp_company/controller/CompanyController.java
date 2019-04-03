7ad8a050382fcba3f86ae17dec0154fd41a39a22
zcy960312 <zcy960312@gmail.com>
2019-04-02 21:46:10 -0400

    integrate the service

--- src/main/java/com/example/yp_company/controller/CompanyController.java
+++ src/main/java/com/example/yp_company/controller/CompanyController.java
@@ -21,6 +21,7 @@ public class CompanyController {
     @RequestMapping(value = "/search", method = RequestMethod.POST)
     @ResponseBody
     public List<Company> processQuery(@RequestParam String query){
+        if(query == null || query =="") return new ArrayList<Company>();
         String[] subqueries = query.split("&");
         String state = "";
         String city = "";
